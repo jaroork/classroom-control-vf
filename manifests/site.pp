be080c7a50a037c63e7842a2101dc6c64c585e55
jaroork <jroork1@gmail.com>
2016-10-25 15:05:20 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -51,7 +51,7 @@ node default {
 #  content => "I learned how to add a file resource to site.pp\n",
 #  }
 
-exec {'cowsay "Welcome to ${::fqdn}!" > /etc/motd':
+exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
   path => '/usr/local/bin/',
   creates => '/etc/motd',
   }
