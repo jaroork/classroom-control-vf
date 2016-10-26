045983e14dd09e9c8ec3eb70828c5fc4a04deb37
jaroork <jroork1@gmail.com>
2016-10-26 15:28:15 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,6 +4,37 @@ package { 'nginx':
   ensure => present,
 }
 
+file { '/etc/nginx/nginx.conf':
+  ensure => 'file',
+  group => 'root',
+  owner => 'root',
+  mode => '0644',
+  source => 'puppet:///modules/nginx/nginx.conf',
+  require => Package['nginx']
+}
 
+file { '/var/www/index.html':
+  ensure => 'file',
+  group => 'root',
+  owner => 'root',
+  mode => '0644',
+  source => 'puppet:///modules/nginx/index.html',
+  require => Package['nginx']
+}
+
+file { '':
+  ensure => 'file',
+  group => 'root',
+  owner => 'root',
+  mode => '0644',
+  source => 'puppet:///modules/nginx/default.conf',
+  require => Package['nginx']
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  subscribe => File['/etc/nginx/nginx.conf'],
+}
 
 }
