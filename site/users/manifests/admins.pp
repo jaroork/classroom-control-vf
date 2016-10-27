301efbf2e2187d59b963e93e3b91595013733ccf
jaroork <jroork1@gmail.com>
2016-10-27 14:55:53 -0500

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,10 +1,10 @@
 class users::admins {
-  users::managed_user { 'joe':
+  users::managed_user { 'jose':
   }
   users::managed_user { 'alice':
     group => 'staff',
   }
-  users::managed_user { 'aaron':
+  users::managed_user { 'chen':
     group => 'staff',
   }
   group { 'staff':
