27b2abdf304107ea269e1d32fef1032ac1aecccb
jaroork <jroork1@gmail.com>
2016-10-27 10:10:38 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,50 +1,35 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    ensure => 'file',
+    require => Package['nginx'],
+  }
+  
+  package { 'nginx':
+    ensure => present,
+  }
 
-package { 'nginx':
-  ensure => present,
-}
+  file { '/etc/nginx/nginx.conf':
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
 
-file {'/var/www':
-  ensure => 'directory',
-  owner => 'root',
-  group => 'root',
-  mode => '0775',
-}
+  file { '/var/www/index.html':
+    source => 'puppet:///modules/nginx/index.html',
+  }
 
-file { '/etc/nginx/nginx.conf':
-  ensure => 'file',
-  group => 'root',
-  owner => 'root',
-  mode => '0644',
-  source => 'puppet:///modules/nginx/nginx.conf',
-  require => Package['nginx'],
-  notify => Service['nginx'],
-}
-
-file { '/var/www/index.html':
-  ensure => 'file',
-  group => 'root',
-  owner => 'root',
-  mode => '0775',
-  source => 'puppet:///modules/nginx/index.html',
-  require => Package['nginx'],
-  notify => Service['nginx'],
-}
-
-file { '/etc/nginx/conf.d/default.conf':
-  ensure => 'file',
-  group => 'root',
-  owner => 'root',
-  mode => '0644',
-  source => 'puppet:///modules/nginx/default.conf',
-  require => Package['nginx'],
-  notify => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
-}
-
+  file { '/etc/nginx/conf.d/default.conf':
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  file { '/var/www':
+    ensure => 'directory',
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => [File['/etc/nginx/nginx.conf'],File['/etc/nginx/conf.d/default.conf']],
+  }
 }
