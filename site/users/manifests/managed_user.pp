bd3aac5bdbdf27c07e8063a0935f612604f788ca
jaroork <jroork1@gmail.com>
2016-10-27 15:17:35 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -8,5 +8,16 @@ define users::managed_user (
     ensure => directory,
     owner => $title,
     group => $group,
-    }
   }
+  file { "/home/${title}/.ssh":
+    ensure => directory,
+    owner => $title,
+    mode => '700',
+  }
+  file { "/home/${title}/.bashrc":
+    ensure => file,
+    owner => $title,
+    mode => '644',
+    source => 'puppet:///modules/users/bashrc',
+  }
+}
