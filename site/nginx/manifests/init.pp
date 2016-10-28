c6ba66a532b7556bec1df46c325ddb0e44ab464d
jaroork <jroork1@gmail.com>
2016-10-28 10:20:01 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,50 +1,66 @@
+# nginx/manifests/init.pp
 class nginx {
-  
+
   case $::osfamily {
-    'RedHat': {
+    'RedHat','Debian': {
       $docroot = '/var/www'
-      $logdir = '/var/log/nginx'
+      $logsdir = '/var/log/nginx'
       $confdir = '/etc/nginx'
-      $blockdir = '/etc/nginx/conf.d'
+      $blckdir = '/etc/nginx/conf.d'
+      $pkgname = 'nginx'
+      $fileown = 'root'
+      $filegrp = 'root'
+      $svcname = 'nginx'
+    }
+    'windows': {
+      $docroot = 'C:/ProgramData/nginx/html'
+      $logsdir = 'C:/ProgramData/nginx/logs'
+      $confdir = 'C:/ProgramData/nginx'
+      $blckdir = 'C:/ProgramData/nginx/conf.d'
+      $pkgname = 'nginx-service'
+      $fileown = 'Administrator'
+      $filegrp = 'Administrators'
+      $svcname = 'nginx'
     }
   }
-  
-  $user = $::osfamily ? {
-    'Redhat' => 'nginx'
+ 
+  $svcuser = $::osfamily ? {
+    'RedHat'  => 'nginx',
+    'Debian'  => 'www-data',
+    'windows' => 'nobody',
   }
   
   File {
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    ensure => 'file',
-    require => Package['nginx'],
+    ensure => file,
+    owner  => $fileown,
+    group  => $filegrp,
+    mode   => '0644',
+  }
+
+  package { $pkgname:
+    ensure => present,
+    before => [File["${blckdir}/default.conf"],File["${confdir}/nginx.conf"]],
   }
   
-  package { 'nginx':
-    ensure => present,
-    before => [File["${blockdir}/default.conf"],File["${confdir}/nginx.conf"]],
+  file { $docroot: 
+    ensure => directory,
   }
-
-  file { "${confdir}/nginx.conf":
-    content  => epp('nginx/nginx.conf.epp'),
-  }
-
+  
   file { "${docroot}/index.html":
     source => 'puppet:///modules/nginx/index.html',
   }
-
-  file { "${blockdir}/default.conf":
+  
+  file { "${blckdir}/default.conf":
     content  => epp('nginx/default.conf.epp'),
   }
   
-  file { $docroot:
-    ensure => 'directory',
-  }
-  
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => [File["${blockdir}/default.conf"],File["${confdir}/nginx.conf"]],
+  file { "${confdir}/nginx.conf":
+    content  => epp('nginx/nginx.conf.epp'),
+}
+    
+  service { $svcname:
+    ensure    => running,
+    enable    => true,
+    subscribe => [File["${blckdir}/default.conf"],File["${confdir}/nginx.conf"]],
   }
 }
