50b14bf37ef4b98425ca6dc1740b1fe9ebae8fec
jaroork <jroork1@gmail.com>
2016-10-28 11:46:06 -0500

    Create params.pp

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,36 @@
+class nginx::params (
+  $root = undef,
+) {
+  case $::osfamily {
+    'RedHat','Debian': {
+      $default_docroot = '/var/www'
+      $logsdir = '/var/log/nginx'
+      $confdir = '/etc/nginx'
+      $blckdir = '/etc/nginx/conf.d'
+      $pkgname = 'nginx'
+      $fileown = 'root'
+      $filegrp = 'root'
+      $svcname = 'nginx'
+    }
+    'windows': {
+      $default_docroot = 'C:/ProgramData/nginx/html'
+      $logsdir = 'C:/ProgramData/nginx/logs'
+      $confdir = 'C:/ProgramData/nginx'
+      $blckdir = 'C:/ProgramData/nginx/conf.d'
+      $pkgname = 'nginx-service'
+      $fileown = 'Administrator'
+      $filegrp = 'Administrators'
+      $svcname = 'nginx'
+    }
+  }
+ 
+  $svcuser = $::osfamily ? {
+    'RedHat'  => 'nginx',
+    'Debian'  => 'www-data',
+    'windows' => 'nobody',
+  }
+  
+  $docroot = $root ? {
+    undef => $default_docroot,
+    default => $root,
+  }
